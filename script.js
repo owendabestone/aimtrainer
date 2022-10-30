c02efc7aa7444bc27d38356624693953c06b7fbf
owendabestone <chz230@ucsd.edu>
2022-10-30 14:44:28 -0600

    update 1

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ class ballon{
     }
     
 }
+var ids = []
 var score = 0;
 var number_balloons = 0;
 
@@ -39,7 +40,7 @@ document.addEventListener("keydown", function(event) {
 
 function reset(){
     document.getElementById("score").innerHTML = "Score: " + 0;
-    make_ballons()
+    location.reload();
 }
 function sleep(miliseconds) {
     var currentTime = new Date().getTime();
@@ -52,7 +53,8 @@ function sleep(miliseconds) {
     return new Promise(resolve => setTimeout(resolve, time));
   }
 (async function make_ballons(){
-    for (var i = 1; i < 10000; i++) {
+
+    for (var i = 1; i < 100; i++) {
         pos1 = 20+ Math.random()*60 ;
         pos2 = 20+ Math.random()*60;
         id_ = number_balloons;
@@ -61,10 +63,10 @@ function sleep(miliseconds) {
         ballon_html = balloon_.display();
         document.getElementById("balloons").appendChild(ballon_html);
         number_balloons ++;
-        await delay(1000);
-
+        await delay(250);
+    
     }
-
+    alert("You have scored: " + score)
 })()
 
 function delete_(id){
